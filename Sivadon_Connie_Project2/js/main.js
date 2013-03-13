e55acd2cdf63920013f1474d5cefc265f8f7dada
csivadon <csivadon@fullsail.edu>
2013-03-13 20:36:07 +0100

    worked on some of my variables in js.

--- Sivadon_Connie_Project2/js/main.js
+++ Sivadon_Connie_Project2/js/main.js
@@ -3,4 +3,29 @@
 // 14 March 2013
 // Project 1 main.js
 
-var search = document.getElementById("button");
+// DOM Function
+window.addEventListener("DOMContentLoaded", function(){
+	
+	
+	
+	
+	
+	//getElementById Function	
+	function $(x){
+	var theElement = document.getElementById(x);
+	return theElement;
+	}
+	
+	
+	
+	
+	//Variable default
+	var time = ["--What time of day would you like this meeting to take place?--", "morning", "afternoon", "evening"];
+
+	//Set Link & Submit Click Events
+	var displayData = $("Show meeting requests");
+	displayData.addEventListener("click", getData);
+	var clearData = $("clear");
+	clearData.addEventListener("click", clearData);
+	var request = $("button");
+});
