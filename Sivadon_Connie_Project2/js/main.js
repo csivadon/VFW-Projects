d5b51242430353346783ec3e78e5766d86b75b29
csivadon <csivadon@fullsail.edu>
2013-03-13 23:31:54 +0100

    created two functions in js file.

--- Sivadon_Connie_Project2/js/main.js
+++ Sivadon_Connie_Project2/js/main.js
@@ -16,16 +16,48 @@ window.addEventListener("DOMContentLoaded", function(){
 	return theElement;
 	}
 	
+	//Select Field Elements
+	function createElements(){
+		var formTag = document.getElementsByTagName("form"),
+			selectList = $("timeSelect"),
+			makeSel = document.createElement("select");
+			makeSel.setAttribute("id", "time");
+		for(var i=0, j=timeOfDay.length; i<j; i++){
+			var makeOpt = document.createElement("option");
+			var optionTxt = timeOfDay[i];
+			makeOpt.setAttribute("value", optionTxt);
+			makeOpt.innerHTML = optionTxt;
+			makeSel.appendChild(makeOpt);
+		}
+		selectList.appendChild(makeSel);
+	}
 	
-	
-	
+	function storeData(){
+		var id			= Math.floor(Math.random()*100000001);
+		var item		= {};
+		    item.gTag		= ["Gamer Tag:", $("gTag").value];
+		    item.email		= ["Email:", $("email").value];
+		    item.url		= ["Url:", $("url").value];
+		    item.date		= ["Date:", $("date").value];
+		    // item.meetingType	= ["Meeting Type:", meetingType.value];
+		    // item.game		= ["Game:", gameValue];
+		    item.time		= ["Time of Day:", $("time").value];
+		    item.howLong	= ["For How Long:", $("howLong").value];
+		    item.moreInfo	= ["More Info:", $("moreInfo").value];
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Meeting Requested!");
+	}
 	//Variable default
-	var time = ["--What time of day would you like this meeting to take place?--", "morning", "afternoon", "evening"];
-
+	var timeOfDay = ["--Choose Time Frame--", "morning", "afternoon", "evening"];
+	
 	//Set Link & Submit Click Events
-	var displayData = $("Show meeting requests");
+	/*var displayData = $("Show meeting requests");
 	displayData.addEventListener("click", getData);
 	var clearData = $("clear");
-	clearData.addEventListener("click", clearData);
+	clearData.addEventListener("click", clearData);*/
 	var request = $("button");
+	request.addEventListener("click", storeData);
+	
+	//Function Calls
+	createElements();
 });
